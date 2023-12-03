0a53c9633bdb032132e781c52e7d37d2519df6b5
Ibrahim <inobani28@gmail.com>
2023-12-03 17:56:21 +0200

    Adding the model DTO needed for creating and updating the models

---
+++ RestaurantReservationRestfulAPI/ModelsDto/UpdateRestaurantDto.cs
@@ -0,0 +1,10 @@
+namespace RestaurantReservation.API.ModelsDto
+{
+    public class UpdateRestaurantDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string OpeningHours { get; set; }
+    }
+}
