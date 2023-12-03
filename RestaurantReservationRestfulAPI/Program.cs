75e2cdaa0cf7c31bc914d35e2aebfe63fa5c6873
Ibrahim <inobani28@gmail.com>
2023-12-03 18:01:10 +0200

    Adding the main program with its extensions and the appsettings.json

--- RestaurantReservationRestfulAPI/Program.cs
+++ RestaurantReservationRestfulAPI/Program.cs
@@ -1,5 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
+
+builder.Services.AddCustomServices();
+builder.Services.AddCustomDbContext(builder.Configuration);
+builder.Services.AddCustomJWT(builder.Configuration);
+builder.Services.AddCustomMvc();
+builder.Services.AddSwagger();
+
+
 var app = builder.Build();
+
+app.UseCustomSwagger();
+app.UseCustomMiddleware();
+
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
