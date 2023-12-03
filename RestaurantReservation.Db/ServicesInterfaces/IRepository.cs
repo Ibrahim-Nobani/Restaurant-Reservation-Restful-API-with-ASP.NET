c8537dca9859b0f0a132779fd9411c9ad8e624c0
Ibrahim <inobani28@gmail.com>
2023-12-03 17:49:32 +0200

    Adding Interfaces for the repositroy pattern

---
+++ RestaurantReservation.Db/ServicesInterfaces/IRepository.cs
@@ -0,0 +1,11 @@
+namespace RestaurantReservation.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
