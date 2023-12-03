54eaf65a33d6f09c10a5bd920c0180f1629f75dc
Ibrahim <inobani28@gmail.com>
2023-12-03 17:48:56 +0200

    Adding dbcontext and migrations

---
+++ RestaurantReservation.Db/Migrations/20231016172728_AddStoredProcedureFindCustomersByPartySize.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStoredProcedureFindCustomersByPartySize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                    CREATE PROCEDURE sp_FindCustomersByPartySize
+                        @PartySize INT
+                    AS
+                    BEGIN
+                        SELECT c.*
+                        FROM Customers c
+                        INNER JOIN Reservations r
+                        on r.CustomerId = c.CustomerId
+                        WHERE r.PartySize > @PartySize
+                    END
+                ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS sp_FindCustomersByPartySize");
+        }
+    }
+}
